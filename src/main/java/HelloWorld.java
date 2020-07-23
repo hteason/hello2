e1f33e52504769342e8bb8f46ccfb1977b4f3864
huangtingcheng <huangtingcheng@banggood.com>
2020-07-23 09:57:49 +0800

    Revert "first update"
    This reverts commit 6f8d6eb0

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! --add content");
+        System.out.println("Hello World!");
     }
 }
